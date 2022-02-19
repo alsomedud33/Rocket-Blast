40330777ad7b2642e876e19b9cc3ac38301234c1
alsomedud33 <Alsomedude@hotmail.com>
2022-02-19 03:06:43 +0000

    performance adjustments
    explosion particle effect adjustment

--- SpatialGranade.gd
+++ SpatialGranade.gd
@@ -4,7 +4,7 @@ var frame = 0
 var duration = 5
 export var speed:int = 10
 var velocity = Vector3()
-
+var bounce
 export var explosion : PackedScene
 
 onready var main = get_tree().current_scene
@@ -17,7 +17,7 @@ func _ready():
 
 func _physics_process(delta):
 	velocity = move_and_slide(velocity, Vector3.UP,false, 4, PI/4, false)
-	var bounce = move_and_collide(velocity * delta)
+	bounce = move_and_collide(velocity * delta)
 	if bounce:
 		self.rotation = Vector3(velocity.bounce(bounce.normal).x,velocity.bounce(bounce.normal).y,velocity.bounce(bounce.normal).z * 1.5)
 		velocity = velocity.bounce(bounce.normal) * 0.8
@@ -27,12 +27,13 @@ func _physics_process(delta):
 		explosion_instance.transform.looking_at(-velocity.bounce(bounce.normal), Vector3.UP)
 		#print("bye")
 		queue_free()
-	for index in get_slide_count():
-		var collision = get_slide_collision(index)
-		main.add_child(explosion_instance)
-		explosion_instance.global_transform.origin = collision.get_position()
-		
-		queue_free()
+	else:
+		for index in get_slide_count():
+			var collision = get_slide_collision(index)
+			main.add_child(explosion_instance)
+			explosion_instance.global_transform.origin = collision.get_position()
+			
+			queue_free()
 		#collision.normal
 	#velocity = move_and_collide(velocity * delta)
 func _on_Area_area_entered(area):
